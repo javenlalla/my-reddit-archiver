d95c9f3b89243875b46eaef3e25348f48d05867a
Javen Lalla <javen.lalla@gmail.com>
2022-09-11 09:46:32 +0200

    fix: implement proper logging in rate limit subscriber

--- api/app/src/EventSubscriber/RateLimiterSubscriber.php
+++ api/app/src/EventSubscriber/RateLimiterSubscriber.php
@@ -3,12 +3,17 @@
 namespace App\EventSubscriber;
 
 use App\Event\RedditApiCallEvent;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\RateLimiter\RateLimiterFactory;
 
+/**
+ * Event Subscriber to track calls to the Reddit API in order to avoid exceeding
+ * the rate limit.
+ */
 class RateLimiterSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private readonly RateLimiterFactory $redditApiLimiter)
+    public function __construct(private readonly RateLimiterFactory $redditApiLimiter, private readonly LoggerInterface $logger)
     {
     }
 
@@ -25,12 +30,12 @@ class RateLimiterSubscriber implements EventSubscriberInterface
     public function onApiCall(RedditApiCallEvent $event)
     {
         $limiter = $this->redditApiLimiter->create($event->getUsername());
-
         if (false === $limiter->consume(1)->isAccepted()) {
-            // @TODO: Replace generic messages with proper logging.
-            echo "API Limit reached. Waiting.\n";
+            $this->logger->info('API Limit reached. Waiting.');
+
             $limiter->reserve(1)->wait();
-            echo "Waiting completed. Proceeding.\n";
+
+            $this->logger->info('Waiting completed. Proceeding.');
         }
     }
 }
