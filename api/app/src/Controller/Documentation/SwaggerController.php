142d6adc5377c58a916a885808f15d55ff0aac45
Javen Lalla <javen.lalla@gmail.com>
2022-12-10 22:46:40 +0100

    feat: create placeholder template for swagger ui documentation

---
+++ api/app/src/Controller/Documentation/SwaggerController.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Documentation;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/documentation', name: 'documentation_')]
+class SwaggerController extends AbstractController
+{
+    #[Route('/swagger', name: 'swagger')]
+    public function swaggerDocs(): Response
+    {
+        // get the user information and notifications somehow
+        $userFirstName = '...';
+        $userNotifications = ['...', '...'];
+
+        // the template path is the relative file path from `templates/`
+        return $this->render('swagger.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'user_first_name' => $userFirstName,
+            'notifications' => $userNotifications,
+        ]);
+    }
+}
