06f4142da8f88e20ad4ec73f2bf3f2483e8d865f
Javen Lalla <javen.lalla@gmail.com>
2022-12-16 23:51:08 +0100

    feat: rebuild dev env using ubuntu buster image
    This refactor opens up the possibility to use a search engine within the
    container. In earlier testing and research, this proved difficult or
    not-doable with an Alpine image, hence the change.

--- development/entrypoint.dev.sh
+++ development/entrypoint.dev.sh
@@ -23,7 +23,16 @@ DB_USERNAME=${DB_USERNAME:-my_archiver}
 DB_PASSWORD=${DB_PASSWORD:-my_archiver_password}
 DB_PORT=${DB_PORT:-3306}
 
-# Configure the DATABASE_URL Environment Variabled needed by the application.
+# Configure the .env variables.
+#
+# Note: it is intentional that the APP_ENV is written to the .env file instead of declared in the Dockerfile.dev because the container Environment Variables take precedence over the .env file(s).
+# As a result, running tests in the development environment fail due to not switching to the test environment programmatically when running. The following error is thrown:
+# Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException: You have requested a non-existent service "test.service_container". Did you mean this: "service_container"?
+# So to address this and have the command line switch environments correctly during tests, the APP_ENV variable is written to the .env file.
+# See the following page for more info: https://symfony.com/doc/current/configuration.html#overriding-environment-values-via-env-local
+# "Real environment variables always win over env vars created by any of the .env files."
+echo "" > .env
+echo "APP_ENV=dev" >> .env
 export DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?serverVersion=mariadb-10.8.6&charset=utf8mb4"
 echo "DATABASE_URL=${DATABASE_URL}" >> .env
 
@@ -53,6 +62,4 @@ php bin/console app:persist-reddit-account
 
 # Container is set up. Start services.
 echo "Starting services."
-redis-server --daemonize yes
-nginx
-php-fpm
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
