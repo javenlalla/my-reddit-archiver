31fbc14aaeeb1c2afb09a7d9b4e81279ec53cb4c
Javen Lalla <javen.lalla@gmail.com>
2022-12-18 00:07:01 +0100

    refactor: combine main and dev entrypoint files into one

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,10 +23,20 @@ DB_USERNAME=${DB_USERNAME:-my_archiver}
 DB_PASSWORD=${DB_PASSWORD:-my_archiver_password}
 DB_PORT=${DB_PORT:-3306}
 
-# Configure the DATABASE_URL Environment Variabled needed by the application.
+# Configure the .env variables.
 export DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?serverVersion=mariadb-10.8.6&charset=utf8mb4"
 > .env
 echo "DATABASE_URL=${DATABASE_URL}" >> .env
+
+if [ $APP_ENV != "prod" ]; then
+    # Note: it is intentional that the APP_ENV is written to the .env file instead of declared in the Dockerfile.dev because the container Environment Variables take precedence over the .env file(s).
+    # As a result, running tests in the development environment fail due to not switching to the test environment programmatically when running. The following error is thrown:
+    # Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException: You have requested a non-existent service "test.service_container". Did you mean this: "service_container"?
+    # So to address this and have the command line switch environments correctly during tests, the APP_ENV variable is written to the .env file.
+    # See the following page for more info: https://symfony.com/doc/current/configuration.html#overriding-environment-values-via-env-local
+    # "Real environment variables always win over env vars created by any of the .env files."
+    echo "APP_ENV=dev" >> .env
+fi
 
 # Configure Typesense.
 TYPESENSE_API_KEY=$(openssl rand -base64 40 | tr -d /=+ | cut -c -32)
@@ -39,12 +49,17 @@ echo "data-dir = /etc/typesense/typesense-data" >> /etc/typesense/typesense-conf
 echo "enable-cors = true" >> /etc/typesense/typesense-config.ini
 
 # Install and configure composer dependencies.
-echo "Installing composer dependencies."
-composer install --prefer-dist --no-dev --no-autoloader --no-scripts --no-progress
-composer clear-cache
-composer dump-autoload --classmap-authoritative --no-dev
-composer dump-env prod
-composer run-script --no-dev post-install-cmd
+if [ $APP_ENV = "prod" ]; then
+    echo "Installing composer dependencies."
+    composer install --prefer-dist --no-dev --no-autoloader --no-scripts --no-progress
+    composer clear-cache
+    composer dump-autoload --classmap-authoritative --no-dev
+    composer dump-env prod
+    composer run-script --no-dev post-install-cmd
+else
+  echo "Installing composer dependencies. This will take a few minutes since xdebug is installed."
+  composer install
+fi
 
 # Wait for the database to be accessible before proceeding.
 echo "Attempting to reach database ${DB_HOST}:${DB_PORT} with user ${DB_USERNAME}."
