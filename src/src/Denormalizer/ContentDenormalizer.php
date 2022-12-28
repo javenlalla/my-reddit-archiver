7d101f9e865169b36753afaa4b89207f9e2f9970
Javen Lalla <javen.lalla@gmail.com>
2022-12-28 22:23:31 +0100

    refactor: separate the post and comment logic in the content denormalizer

--- src/src/Denormalizer/ContentDenormalizer.php
+++ src/src/Denormalizer/ContentDenormalizer.php
@@ -56,36 +56,17 @@ class ContentDenormalizer implements DenormalizerInterface
             $data = $data['data']['children'][0];
         }
 
-        if ($data['kind'] === Kind::KIND_COMMENT) {
-            $kind = $this->kindRepository->getCommentType();
-        } else {
-            $kind = $this->kindRepository->getLinkType();
-        }
+        $kindRedditId = $data['kind'];
+        $kind = $this->getKindFromKindId($kindRedditId);
         $context['kind'] = $kind;
 
-        if ($data['kind'] === Kind::KIND_LINK) {
-            $post = $this->linkPostDenormalizer->denormalize($data['data'], Post::class, null, $context);
-        } elseif ($data['kind'] === Kind::KIND_COMMENT) {
-            $post = $this->linkPostDenormalizer->denormalize($context['parentPostData']['data']['children'][0]['data'], Post::class, null, $context);
-        } else {
-            throw new Exception(sprintf('Unexpected Post type %s: %s', $data['kind'], var_export($data, true)));
-        }
+        $post = $this->getDenormalizedPost($kindRedditId, $data, $context);
 
-        $existingPost = $this->postRepository->findOneBy(['redditId' => $post->getRedditId()]);
-        if (!empty($existingPost)) {
-            $post = $existingPost;
-        }
-
-        $comment = null;
-        if (!empty($context['commentData'])) {
-            // @TODO: Is this additional Kind check needed here?
+        $comment = $this->getDenormalizedComment($post, $context);
+        if ($comment instanceof Comment) {
+            // This is currently needed to force the correct Kind on the Content
+            // for Comment Contents.
             $kind = $this->kindRepository->getCommentType();
-            $comment = $this->commentDenormalizer->denormalize($post, Comment::class, null, $context);
-
-            $existingComment = $this->commentRepository->findOneBy(['redditId' => $comment->getRedditId()]);
-            if (!empty($existingComment)) {
-                $comment = $existingComment;
-            }
         }
 
         return $this->getNewOrExistingContent($kind, $post, $comment);
@@ -123,5 +104,76 @@ class ContentDenormalizer implements DenormalizerInterface
         }
 
         return $content;
+    }
+
+    /**
+     * Retrieve the Kind Entity associated to the provided Kind Reddit ID.
+     *
+     * @param  string  $kindRedditId Kind Reddit ID `t1` or `t3`.
+     *
+     * @return Kind
+     */
+    private function getKindFromKindId(string $kindRedditId): Kind
+    {
+        if ($kindRedditId === Kind::KIND_COMMENT) {
+            $kind = $this->kindRepository->getCommentType();
+        } else {
+            $kind = $this->kindRepository->getLinkType();
+        }
+
+        return $kind;
+    }
+
+    /**
+     * Retrieve the denormalized Post associated to the provided Response Data
+     * for this Content.
+     *
+     * @param  string  $kindRedditId
+     * @param  array  $responseData
+     * @param  array  $context
+     *
+     * @return Post
+     * @throws Exception
+     */
+    private function getDenormalizedPost(string $kindRedditId, array $responseData, array $context): Post
+    {
+        if ($kindRedditId === Kind::KIND_LINK) {
+            $post = $this->linkPostDenormalizer->denormalize($responseData['data'], Post::class, null, $context);
+        } elseif ($kindRedditId === Kind::KIND_COMMENT) {
+            $post = $this->linkPostDenormalizer->denormalize($context['parentPostData']['data']['children'][0]['data'], Post::class, null, $context);
+        } else {
+            throw new Exception(sprintf('Unexpected Post type %s: %s', $kindRedditId, var_export($responseData, true)));
+        }
+
+        $existingPost = $this->postRepository->findOneBy(['redditId' => $post->getRedditId()]);
+        if (!empty($existingPost)) {
+            $post = $existingPost;
+        }
+
+        return $post;
+    }
+
+    /**
+     * Retrieve the denormalized Comment, if any, associated to the provided
+     * `context`.
+     *
+     * @param  Post  $post
+     * @param  array  $context
+     *
+     * @return Comment|null
+     */
+    private function getDenormalizedComment(Post $post, array $context): ?Comment
+    {
+        $comment = null;
+        if (!empty($context['commentData'])) {
+            $comment = $this->commentDenormalizer->denormalize($post, Comment::class, null, $context);
+
+            $existingComment = $this->commentRepository->findOneBy(['redditId' => $comment->getRedditId()]);
+            if (!empty($existingComment)) {
+                $comment = $existingComment;
+            }
+        }
+
+        return $comment;
     }
 }
