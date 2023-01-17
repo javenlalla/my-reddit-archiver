e6204ae601c5accd57cf41099345149556b9b71c
Javen Lalla <javen.lalla@gmail.com>
2023-01-17 08:40:43 +0100

    build: add command to server script to hook into dev container

--- server.sh
+++ server.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #
 # Usage
-#   - ./server.sh start-dev | Start dev server for local development environment.
-#   - ./server.sh stop-dev | Stop dev server.
+#   - ./server.sh start-dev | Start the dev container(s) for local development environment.
+#   - ./server.sh stop-dev | Stop dev container(s).
+#   - ./server.sh connect-dev | Hook into the dev container.
 
 action=$1
 
@@ -10,6 +11,8 @@ if [[ "${action}" = "start-dev" ]]; then
     docker-compose -f docker-compose.development.yml up -d
 elif [[ "${action}" = "stop-dev" ]]; then
     docker-compose -f docker-compose.development.yml stop
+elif [[ "${action}" = "connect-dev" ]]; then
+    docker exec -it mra-dev bash
 else
   echo "Invalid `action` argument provided. See usage details."
   exit 1
