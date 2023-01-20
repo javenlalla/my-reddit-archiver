95dabc9ae04715b4c507bd5ff5331529040fe8ce
Javen Lalla <javen.lalla@gmail.com>
2023-01-20 22:50:33 +0100

    refactor: re-use comment denormalizer in commentsandmore denormalizer

--- src/src/Denormalizer/CommentsAndMoreDenormalizer.php
+++ src/src/Denormalizer/CommentsAndMoreDenormalizer.php
@@ -1,26 +1,21 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Denormalizer;
 
-use App\Entity\AuthorText;
 use App\Entity\Award;
 use App\Entity\Comment;
-use App\Entity\CommentAuthorText;
 use App\Entity\CommentAward;
 use App\Entity\Post;
-use App\Helper\SanitizeHtmlHelper;
-use App\Repository\CommentRepository;
 use App\Service\Reddit\Api;
-use DateTimeImmutable;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 class CommentsAndMoreDenormalizer implements DenormalizerInterface
 {
     public function __construct(
+        private readonly CommentDenormalizer $commentDenormalizer,
         private readonly Api $api,
-        private readonly CommentRepository $commentRepository,
-        private readonly SanitizeHtmlHelper $sanitizeHtmlHelper,
         private readonly AwardDenormalizer $awardDenormalizer,
     ){
     }
@@ -49,29 +44,7 @@ class CommentsAndMoreDenormalizer implements DenormalizerInterface
                 $commentData = $commentRawData['data'];
             }
 
-            $existingComment = $this->commentRepository->findOneBy(['redditId' => $commentData['id']]);
-            if (!empty($existingComment)) {
-                $comment = $existingComment;
-            } else {
-                // @TODO: This is repetitive logic. Replace with a call to the CommentDenormalizer.
-                $comment = new Comment();
-                $comment->setRedditId($commentData['id']);
-                $comment->setScore((int) $commentData['score']);
-                $comment->setAuthor($commentData['author']);
-                $comment->setParentPost($context['post']);
-                $comment->setDepth((int) $commentData['depth']);
-
-                $authorText = new AuthorText();
-                $authorText->setText($commentData['body']);
-                $authorText->setTextRawHtml($commentData['body_html']);
-                $authorText->setTextHtml($this->sanitizeHtmlHelper->sanitizeHtml($commentData['body_html']));
-
-                $commentAuthorText = new CommentAuthorText();
-                $commentAuthorText->setAuthorText($authorText);
-                $commentAuthorText->setCreatedAt(DateTimeImmutable::createFromFormat('U', $commentData['created_utc']));
-
-                $comment->addCommentAuthorText($commentAuthorText);
-            }
+            $comment = $this->commentDenormalizer->denormalize($context['post'], Comment::class, null, ['commentData' => $commentData]);
 
             if (isset($context['parentComment']) && $context['parentComment'] instanceof Comment) {
                 $comment->setParentComment($context['parentComment']);
