d7dbfa0607bad2ceaf9b0813358381f55a3e188a
Javen Lalla <javen.lalla@gmail.com>
2023-01-28 12:51:21 +0100

    refactor: convert subreddit and flair search fields to entity types

--- src/src/Form/SearchForm.php
+++ src/src/Form/SearchForm.php
@@ -3,54 +3,61 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use App\Entity\Post;
+use App\Entity\Subreddit;
 use App\Entity\Tag;
 use App\Repository\PostRepository;
 use App\Repository\SubredditRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SearchForm extends AbstractType
 {
-    public function __construct(
-        private readonly PostRepository $postRepository,
-        private readonly SubredditRepository $subredditRepository,
-        private readonly RouterInterface $router,
-    ) {
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('query', SearchType::class, [
                 'attr' => [
                     'placeholder' => 'Search Archive',
-                    'data-model' => 'query',
-                    // 'data-search-posts-target' => 'query',
-                    // 'data-action' => 'input->search-posts#execSearch',
+                    'data-model' => 'debounce(200)|query',
                 ],
-                'required' => false,
+                'required' => true,
+                'constraints' => [
+                    new Length(['min' => 3]),
+                    new NotBlank(),
+                ],
             ])
-            ->add('subreddits', ChoiceType::class, [
+            ->add('subreddits', EntityType::class, [
+                'class' => Subreddit::class,
+                'choice_label' => 'name',
+                'choice_value' => 'name',
                 'placeholder' => 'Filter By Sub-Reddit',
-                'choices' => $this->getSubredditChoices(),
+                'query_builder' => function (SubredditRepository $repository) {
+                    return $repository->createQueryBuilder('s')
+                        ->orderBy('s.name', 'ASC');
+                },
                 'attr' => [
                     'data-model' => 'subreddit',
-                    // 'data-search-posts-target' => 'subreddit',
-                    // 'data-action' => 'input->search-posts#execSearch',
                 ],
                 'required' => false,
             ])
-            ->add('flairTexts', ChoiceType::class, [
+            ->add('flairTexts', EntityType::class, [
+                'class' => Post::class,
                 'placeholder' => 'Filter By Flair',
-                'choices' => $this->getFlairTextChoices(),
+                'choice_label' => 'flairText',
+                'choice_value' => 'flairText',
+                'query_builder' => function (PostRepository $repository) {
+                    return $repository->createQueryBuilder('p')
+                        ->distinct()
+                        ->where("p.flairText IS NOT NULL AND p.flairText != ''")
+                        ->orderBy('p.flairText');
+                },
                 'attr' => [
                     'data-model' => 'flairText',
-                    // 'data-search-posts-target' => 'flairText',
-                    // 'data-action' => 'input->search-posts#execSearch',
                 ],
                 'required' => false,
             ])
@@ -58,60 +65,11 @@ class SearchForm extends AbstractType
                 'placeholder' => 'Filter By Tag',
                 'class' => Tag::class,
                 'choice_label' => 'name',
-                // 'choices' => $this->getFlairTextChoices(),
                 'attr' => [
                     'data-model' => 'tag',
-                    // 'data-search-posts-target' => 'flairText',
-                    // 'data-action' => 'input->search-posts#execSearch',
                 ],
                 'required' => false,
             ])
-            ->setAction($this->router->generate('search-form'))
-            // ->add('search', SubmitType::class)
         ;
-    }
-
-    /**
-     * Retrieve the list of Subreddits to be used as choices in the Subreddit
-     * Search field.
-     *
-     * @return array
-     */
-    private function getSubredditChoices(): array
-    {
-        $subreddits = $this->subredditRepository->findBy(
-            [],
-            ['name' => 'ASC'],
-        );
-
-        $subredditChoices = [];
-        foreach ($subreddits as $subreddit) {
-            $subredditName = $subreddit->getName();
-            $subredditChoices[$subredditName] = $subredditName;
-        }
-
-        return $subredditChoices;
-    }
-
-    /**
-     * Retrieve the list of Flair Texts to be used as choices in the Flair Text
-     * Search field.
-     *
-     * @return array
-     */
-    private function getFlairTextChoices(): array
-    {
-        $flairTextsData = $this->postRepository->findAllFlairTexts();
-
-        $flairTexts = [];
-        foreach ($flairTextsData as $flairTextData) {
-            $flairText = $flairTextData['flairText'];
-
-            if (!empty($flairText)) {
-                $flairTexts[$flairText] = $flairText;
-            }
-        }
-
-        return $flairTexts;
     }
 }
