6368fdea956dd5931a3a92db6833ca2c8588b0de
Javen Lalla <javen.lalla@gmail.com>
2023-01-28 13:03:33 +0100

    chore: remove live component test code and files

--- src/src/Controller/Debug/TemplateLayoutController.php
+++ src/src/Controller/Debug/TemplateLayoutController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Debug;
 
 use App\Form\SearchForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,50 +25,5 @@ class TemplateLayoutController extends AbstractController
         return $this->render('_sidenav.html.twig', [
             'searchForm' => $searchForm->createView(),
         ]);
-    }
-
-    #[Route('/search-results', name: 'search-results')]
-    public function getSearchResults(Request $request): Response
-    {
-        $posts = [
-            [
-                'id' => 1,
-                'title' => 'First Post',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Second Post',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Third Post',
-            ],
-            [
-                'id' => 4,
-                'title' => 'Fourth Post',
-            ],
-        ];
-
-        $filteredPosts = [];
-        $query = $request->get('q', '');
-        if (!empty($query)) {
-            foreach ($posts as $post) {
-                if (str_contains(strtolower($post['title']), $query)) {
-                    $filteredPosts[] = $post;
-                }
-            }
-        } else {
-            $filteredPosts = $posts;
-        }
-
-        return $this->render('search-results.html.twig', [
-            'posts' => $filteredPosts,
-        ]);
-    }
-
-    #[Route('/live-component', name: 'live-component')]
-    public function getLiveComponent(): Response
-    {
-        return $this->render('live-component.html.twig');
     }
 }
