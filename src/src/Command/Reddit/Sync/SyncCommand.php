cd913fb2d243f348861ad05774156e29c627025a
Javen Lalla <javen.lalla@gmail.com>
2023-07-24 13:38:55 +0200

    feat: ensure the search index is processed after syncing

--- src/src/Command/Reddit/Sync/SyncCommand.php
+++ src/src/Command/Reddit/Sync/SyncCommand.php
@@ -20,6 +20,7 @@ use Exception;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -155,6 +156,8 @@ class SyncCommand extends Command
             $this->reportMemoryUsage($output);
         }
 
+        $this->runSearchIndexing($output);
+
         return Command::SUCCESS;
     }
 
@@ -214,5 +217,20 @@ class SyncCommand extends Command
         $output->writeln(
             'Memory used: ' . $this->getFormattedMemoryUsage()
         );
+    }
+
+    /**
+     * Index all synced Contents for Search.
+     *
+     * @param  OutputInterface  $output
+     *
+     * @return void
+     * @throws Exception
+     */
+    private function runSearchIndexing(OutputInterface $output)
+    {
+        $command = $this->getApplication()->find('reddit:search:index');
+
+        $command->run(new ArrayInput([]), $output);
     }
 }
