974a8bdcec48b914cf0ce7b3a6172064a2621973
Javen Lalla <javen.lalla@gmail.com>
2023-08-29 16:19:38 +0200

    fix: install missing doctrine fixtures dependency

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
