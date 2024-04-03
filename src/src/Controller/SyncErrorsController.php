360498710240c619c185b28d23b67d6900b7c569
Javen Lalla <javen.lalla@gmail.com>
2024-04-03 10:30:52 +0200

    refactor: convert sync errors mgmt live component to normal render

--- src/src/Controller/SyncErrorsController.php
+++ src/src/Controller/SyncErrorsController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\SyncErrorLogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +16,10 @@ class SyncErrorsController extends AbstractController
      * @return Response
      */
     #[Route('/sync-errors', name: 'sync_errors')]
-    public function viewSyncErrors(): Response
+    public function viewSyncErrors(SyncErrorLogRepository $syncErrorLogRepository): Response
     {
-        return $this->render('sync-errors/view.html.twig');
+        return $this->render('sync-errors/view.html.twig', [
+            'errorLogs' => $syncErrorLogRepository->findAll(),
+        ]);
     }
 }
