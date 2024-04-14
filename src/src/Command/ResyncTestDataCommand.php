f523ea31b450522772fa45bab256725d0852c385
Javen Lalla <javen.lalla@gmail.com>
2024-04-14 22:01:30 +0200

    refactor: clean up imports on resync command

--- src/src/Command/ResyncTestDataCommand.php
+++ src/src/Command/ResyncTestDataCommand.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Entity\ItemJson;
-use App\Repository\AssetRepository;
 use App\Repository\ItemJsonRepository;
 use App\Service\Reddit\Api;
-use App\Service\Reddit\Manager\Assets;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
